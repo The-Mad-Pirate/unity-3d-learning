3f16ad13f279685ce4fc70346282b6838a8f2763
madpirate78@gmail.com <madpirate78@gmail.com@8a989d4d-202d-d526-b58d-dd6a7bd0e36b>
2014-02-02 19:52:29 +0000

    Se agregaron texturas a los ladrillos y se mejoro el script de colision tomando en cuenta rotaciones y torques

--- New Unity Project 1/Assets/Scripts/Shooter.cs
+++ New Unity Project 1/Assets/Scripts/Shooter.cs
@@ -3,24 +3,49 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
 	public Rigidbody bullet;
-	public float power = 1500f;
-	public float moveSpeed = 2f;
+	public float power = 2250f;
+	public float moveSpeed = 5f;
+	private Vector3 ballRotationVector = new Vector3();
+	private Vector3 ballAngularVelocity = new Vector3();
+	private const float radius = 1.0f;
+	
+	void Start() {
+		
+		System.Random randGen = new System.Random();
+		
+		ballAngularVelocity.x = (float) (randGen.NextDouble() * 2*Mathf.PI);
+		ballAngularVelocity.z = (float) (randGen.NextDouble() * 2*Mathf.PI);
+		ballAngularVelocity.y = (float) (randGen.NextDouble() * Mathf.PI);		
+		
+	}
 	
 	void Update () {
 		
 		float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
 		float v = Input.GetAxis("Vertical")   * Time.deltaTime * moveSpeed;
+				
+		ballRotationVector.x += ballRotationVector.x + ballAngularVelocity.x * Time.deltaTime;
+		ballRotationVector.y += ballRotationVector.y + ballAngularVelocity.y * Time.deltaTime;
+		ballRotationVector.z += ballRotationVector.z + ballAngularVelocity.z * Time.deltaTime;
 		
+		ballRotationVector.Normalize();
+				
 		transform.Translate(h, v, 0);
 		
 		if( Input.GetButtonUp("Fire1")){
-			Rigidbody instance = Instantiate( bullet, transform.position, transform.rotation) as Rigidbody;
+//			Rigidbody instance = Instantiate( bullet, transform.position, transform.rotation) as Rigidbody;
+			Quaternion rotation = new Quaternion(ballRotationVector.x,
+			                                     ballRotationVector.y,
+			                                     ballRotationVector.z,
+			                                     ballAngularVelocity.magnitude/2*Mathf.PI);
+			Rigidbody instance = Instantiate( bullet, transform.position, rotation) as Rigidbody;
 			Vector3 fwd = transform.TransformDirection(Vector3.forward);
-			instance.AddForce( fwd * power);
-		}
-		
-		
-		
+			
+			instance.MoveRotation(rotation);
+			instance.AddForce( fwd * power );
+			instance.AddTorque( Vector3.Cross(ballAngularVelocity, fwd ) );
+					
 	
+		}
 	}
 }
